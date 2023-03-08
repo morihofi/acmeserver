07a4ef2a8ad8d0d146a3b6b4c5d5523118f4cca3
Moritz Hofmann <info@morihofi.de>
2023-03-08 18:51:26 +0100

    Add database connection function

--- src/main/java/de/morihofi/acmeserver/certificate/Database.java
+++ src/main/java/de/morihofi/acmeserver/certificate/Database.java
@@ -1,5 +1,22 @@
 package de.morihofi.acmeserver.certificate;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Database {
+
+    public static Connection getDatabaseConnection() throws SQLException {
+        String host = "127.0.01";
+        String database = "acmeserver";
+        String user = "root";
+        String password = "empty";
+
+        return DriverManager.getConnection(
+                "jdbc:mariadb://" + host + "/" + database, user, password);
+    }
+
+
+
 
 }
