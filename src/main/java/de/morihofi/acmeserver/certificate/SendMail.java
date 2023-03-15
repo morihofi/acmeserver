211e99c800f418d7c564d95b171e598f7e82a1c3
Moritz Hofmann <info@morihofi.de>
2023-03-15 21:06:33 +0100

    Add logging

--- src/main/java/de/morihofi/acmeserver/certificate/SendMail.java
+++ src/main/java/de/morihofi/acmeserver/certificate/SendMail.java
@@ -3,12 +3,17 @@ package de.morihofi.acmeserver.certificate;
 import de.morihofi.acmeserver.Main;
 import jakarta.mail.*;
 import jakarta.mail.internet.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Properties;
 
 public class SendMail {
 
+    public static final Logger log = LogManager.getLogger(SendMail.class);
+
     public static void sendMail(String toEmail, String subject, String content) throws MessagingException {
+
         Properties emailProp = new Properties();
         emailProp.put("mail.smtp.auth", true);
         if (Main.emailSMTPEncryption.equals("starttls")){
@@ -40,8 +45,9 @@ public class SendMail {
 
         message.setContent(multipart);
 
+        log.info("Sending E-Mail with subject \"" + subject + "\" to \"" + toEmail + "\"");
         Transport.send(message);
-
+        log.info("E-Mail has sent");
 
     }
 }
