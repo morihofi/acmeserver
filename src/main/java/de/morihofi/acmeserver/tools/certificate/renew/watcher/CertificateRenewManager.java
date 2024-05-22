81209be2669baf9eb90dbd007682cd9a405a9f2c
Moritz Hofmann <info@morihofi.de>
2024-05-22 09:37:31 +0200

    chore: Add consistent copyright notice to sources

--- src/main/java/de/morihofi/acmeserver/tools/certificate/renew/watcher/CertificateRenewManager.java
+++ src/main/java/de/morihofi/acmeserver/tools/certificate/renew/watcher/CertificateRenewManager.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2024 Moritz Hofmann <info@morihofi.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ *  subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package de.morihofi.acmeserver.tools.certificate.renew.watcher;
 
 import de.morihofi.acmeserver.certificate.provisioners.Provisioner;
@@ -21,8 +37,8 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class CertificateRenewManager {
-    private static final int period = 6;
-    private static final TimeUnit timeUnit = TimeUnit.HOURS;
+    private static final int PERIOD = 6;
+    private static final TimeUnit TIME_UNIT = TimeUnit.HOURS;
     private static final int RENEWAL_THRESHOLD_DAYS = 7; // Tage vor Ablauf, an denen das Zertifikat erneuert werden soll
     /**
      * Logger
@@ -54,7 +70,7 @@ public class CertificateRenewManager {
     public void startScheduler() {
         LOG.info("Initialized Certificate Renew Scheduler");
         // Start the scheduled task
-        scheduler.scheduleAtFixedRate(this::schedule, 0, period, timeUnit);
+        scheduler.scheduleAtFixedRate(this::schedule, 0, PERIOD, TIME_UNIT);
     }
 
     /**
