75f329d4654210c859b7188bd57b0dbd7c0b225d
Moritz Hofmann <set>
2025-05-30 11:11:21 +0200

    fix: Fix Gson password char-array issue on keystore parsing

--- acmeserver-types/src/main/java/de/morihofi/acmeserver/types/config/helper/KeyStoreParamsDeserializer.java
+++ acmeserver-types/src/main/java/de/morihofi/acmeserver/types/config/helper/KeyStoreParamsDeserializer.java
@@ -16,11 +16,7 @@
 
 package de.morihofi.acmeserver.types.config.helper;
 
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
+import com.google.gson.*;
 import de.morihofi.acmeserver.types.config.keyStoreHelpers.KeyStoreParams;
 import de.morihofi.acmeserver.types.config.keyStoreHelpers.PKCS11KeyStoreParams;
 import de.morihofi.acmeserver.types.config.keyStoreHelpers.PKCS12KeyStoreParams;
@@ -44,16 +40,45 @@ public class KeyStoreParamsDeserializer implements JsonDeserializer<KeyStorePara
      * @return An instance of either {@link PKCS11KeyStoreParams} or {@link PKCS12KeyStoreParams} depending on the 'type' field.
      * @throws JsonParseException if the 'type' field is unknown or missing.
      */
+
     @Override
     @NonNull
-    public KeyStoreParams deserialize(@NonNull JsonElement json, @NonNull Type typeOfT, @NonNull JsonDeserializationContext context) throws JsonParseException {
-        JsonObject jsonObject = json.getAsJsonObject();
-        String type = jsonObject.get("type").getAsString();
+    public KeyStoreParams deserialize(@NonNull JsonElement json,
+                                      @NonNull Type typeOfT,
+                                      @NonNull JsonDeserializationContext context) throws JsonParseException {
 
-        return switch (type.toLowerCase()) {
-            case "pkcs11" -> context.deserialize(json, PKCS11KeyStoreParams.class);
-            case "pkcs12" -> context.deserialize(json, PKCS12KeyStoreParams.class);
-            default -> throw new JsonParseException("Unknown keyStore type: " + type);
+        JsonObject jsonObject = json.getAsJsonObject();
+
+        // extract and convert the password from String or char[] to char[]
+        char[] password = null;
+        if (jsonObject.has("password") && !jsonObject.get("password").isJsonNull()) {
+            JsonElement pwd = jsonObject.get("password");
+
+            if (pwd.isJsonArray()) {                       // still accept the exotic array form
+                JsonArray arr = pwd.getAsJsonArray();
+                password = new char[arr.size()];
+                for (int i = 0; i < arr.size(); i++) {
+                    password[i] = arr.get(i).getAsString().charAt(0);
+                }
+            } else {                                       // normal "password": "s3cr3t"
+                password = pwd.getAsString().toCharArray();
+            }
+        }
+
+        // Remove the field so that Gson’s default adapter will not
+        // attempt to handle char[] on its own and explode again.
+        jsonObject.remove("password");
+
+        // 2. choose the concrete subclass
+        String type = jsonObject.get("type").getAsString();
+        KeyStoreParams params = switch (type.toLowerCase()) {
+            case "pkcs11" -> context.deserialize(jsonObject, PKCS11KeyStoreParams.class);
+            case "pkcs12" -> context.deserialize(jsonObject, PKCS12KeyStoreParams.class);
+            default       -> throw new JsonParseException("Unknown keyStore type: " + type);
         };
+
+        // 3. inject the password we converted
+        params.setPassword(password);
+        return params;
     }
 }
