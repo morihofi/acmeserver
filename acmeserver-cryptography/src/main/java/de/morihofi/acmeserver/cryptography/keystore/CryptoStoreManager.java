d5ff622bc2f81eb0344039d5fde604854f5b484a
Moritz Hofmann <set>
2025-06-04 09:58:24 +0200

    fix: Use switch instead of if's

--- acmeserver-cryptography/src/main/java/de/morihofi/acmeserver/cryptography/keystore/CryptoStoreManager.java
+++ acmeserver-cryptography/src/main/java/de/morihofi/acmeserver/cryptography/keystore/CryptoStoreManager.java
@@ -93,29 +93,31 @@ public class CryptoStoreManager implements ICryptoStoreManager {
             NoSuchMethodException, NoSuchProviderException {
         this.keyStoreConfig = keyStoreConfig;
 
-        if (keyStoreConfig instanceof PKCS11KeyStoreConfig pkcs11Config) {
-            String libraryLocation = pkcs11Config.getLibraryPath().toAbsolutePath().toString();
-
-            log.info("Using PKCS#11 KeyStore with native library at {} with slot {}", libraryLocation, pkcs11Config.getSlot());
-            keyStore = PKCS11KeyStoreLoader.loadPKCS11Keystore(pkcs11Config.getPassword(), pkcs11Config.getSlot(), libraryLocation);
-        }
-        if (keyStoreConfig instanceof PKCS12KeyStoreConfig pkcs12Config) {
-            log.info("Using PKCS#12 KeyStore at {}", pkcs12Config.getPath().toAbsolutePath().toString());
-            keyStore = KeyStore.getInstance("PKCS12", BouncyCastleProvider.PROVIDER_NAME);
-            if (Files.exists(pkcs12Config.getPath())) {
-                // If the file exists, load the existing KeyStore
-                log.info("KeyStore does exist, loading existing into memory");
-                try (InputStream is = Files.newInputStream(pkcs12Config.getPath())) {
-                    keyStore.load(is, pkcs12Config.getPassword());
-                }
-            } else {
-                // Otherwise, initialize a new KeyStore
-                log.info("KeyStore does not exist, creating new KeyStore");
-                keyStore.load(null, pkcs12Config.getPassword());
-
+        switch (keyStoreConfig) {
+            case PKCS11KeyStoreConfig pkcs11Config -> {
+                String libraryLocation = pkcs11Config.getLibraryPath().toAbsolutePath().toString();
+                log.info("Using PKCS#11 KeyStore with native library at {} with slot {}", libraryLocation, pkcs11Config.getSlot());
+                keyStore = PKCS11KeyStoreLoader.loadPKCS11Keystore(
+                        pkcs11Config.getPassword(),
+                        pkcs11Config.getSlot(),
+                        libraryLocation
+                );
             }
+            case PKCS12KeyStoreConfig pkcs12Config -> {
+                log.info("Using PKCS#12 KeyStore at {}", pkcs12Config.getPath().toAbsolutePath().toString());
+                keyStore = KeyStore.getInstance("PKCS12", BouncyCastleProvider.PROVIDER_NAME);
+                if (Files.exists(pkcs12Config.getPath())) {
+                    log.info("KeyStore does exist, loading existing into memory");
+                    try (InputStream is = Files.newInputStream(pkcs12Config.getPath())) {
+                        keyStore.load(is, pkcs12Config.getPassword());
+                    }
+                } else {
+                    log.info("KeyStore does not exist, creating new KeyStore");
+                    keyStore.load(null, pkcs12Config.getPassword());
+                }
+            }
+            default -> throw new IllegalArgumentException("Unsupported key store config type: " + keyStoreConfig.getClass());
         }
-
 
     }
 
