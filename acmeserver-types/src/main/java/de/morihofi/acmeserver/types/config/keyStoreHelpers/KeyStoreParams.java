1a33b624584309147dd508bcc118024c57c85a45
Moritz Hofmann <set>
2025-06-10 14:45:32 +0200

    fix: add missing function

--- acmeserver-types/src/main/java/de/morihofi/acmeserver/types/config/keyStoreHelpers/KeyStoreParams.java
+++ acmeserver-types/src/main/java/de/morihofi/acmeserver/types/config/keyStoreHelpers/KeyStoreParams.java
@@ -18,8 +18,10 @@ package de.morihofi.acmeserver.types.config.keyStoreHelpers;
 
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 /**
  * Abstract base class for KeyStore parameters. This class provides a common structure for different types of KeyStore parameters, allowing
@@ -27,7 +29,21 @@ import java.io.Serializable;
  * to the particular type of KeyStore.
  */
 @Data
+@Slf4j
 public abstract class KeyStoreParams implements Serializable {
     protected String type;
     protected char[] password;
+
+    /**
+     * Clears the password stored in this KeyStoreParams instance.
+     */
+    public void clearPassword() {
+        if (password != null) {
+            Arrays.fill(password, '\0');
+            password = null;
+            log.info("In-Memory config keystore password cleared successfully.");
+        }else {
+            log.warn("Attempted to clear in-memory keystore password, but it was already null.");
+        }
+    }
 }
