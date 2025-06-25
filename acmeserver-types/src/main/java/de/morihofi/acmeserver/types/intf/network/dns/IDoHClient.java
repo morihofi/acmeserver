6fd4c4975d2bd222bcfc1bb50c02c662f135d0fa
Moritz Hofmann <info@morihofi.de>
2025-06-25 19:35:54 +0200

    refactor: Just cleanup

--- acmeserver-types/src/main/java/de/morihofi/acmeserver/types/intf/network/dns/IDoHClient.java
+++ acmeserver-types/src/main/java/de/morihofi/acmeserver/types/intf/network/dns/IDoHClient.java
@@ -1,5 +1,8 @@
 package de.morihofi.acmeserver.types.intf.network.dns;
 
+import org.xbill.DNS.Message;
+import org.xbill.DNS.Record;
+
 import java.io.IOException;
 import java.util.List;
 
@@ -11,6 +14,6 @@ public interface IDoHClient {
      * @return a list of DNS records in the response
      * @throws IOException if an error occurs while sending the query or receiving the response
      */
-    List<org.xbill.DNS.Record> query(org.xbill.DNS.Message query) throws IOException;
+    List<Record> query(Message query) throws IOException;
 
 }
