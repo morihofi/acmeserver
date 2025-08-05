739516cd1b07bb23b6adefe25fbc0fcf61616d13
Moritz Hofmann <info@morihofi.de>
2025-08-05 20:40:13 +0200

    refactor: Merge constructors

--- certgine-cryptography/src/main/java/de/morihofi/certgine/cryptography/tsa/TimeStampAuthority.java
+++ certgine-cryptography/src/main/java/de/morihofi/certgine/cryptography/tsa/TimeStampAuthority.java
@@ -37,7 +37,7 @@ public class TimeStampAuthority {
     private final List<X509Certificate> certificateChain;
     private final ASN1ObjectIdentifier policy;
 
-    private final ASN1ObjectIdentifier TIME_STAMPING_AUTHORITY_POLICY = new ASN1ObjectIdentifier("1.3.6.1.4.1.13762.3");
+    private static final ASN1ObjectIdentifier TIME_STAMPING_AUTHORITY_POLICY = new ASN1ObjectIdentifier("1.3.6.1.4.1.13762.3");
 
     private static final Set<ASN1ObjectIdentifier> ALLOWED_ALGORITHMS = Set.of(
             TSPAlgorithms.MD5,
@@ -66,7 +66,7 @@ public class TimeStampAuthority {
     }
 
     /**
-     * Create a new instance.
+     * Create a new instance with default policy.
      *
      * @param privateKey       signing key
      * @param signingCert      certificate used for signing
@@ -74,10 +74,7 @@ public class TimeStampAuthority {
      */
     public TimeStampAuthority(PrivateKey privateKey, X509Certificate signingCert,
                               List<X509Certificate> certificateChain) {
-        this.privateKey = privateKey;
-        this.signingCert = signingCert;
-        this.certificateChain = certificateChain;
-        this.policy =TIME_STAMPING_AUTHORITY_POLICY;
+        this(privateKey, signingCert, certificateChain, TIME_STAMPING_AUTHORITY_POLICY.getId());
     }
 
     /**
