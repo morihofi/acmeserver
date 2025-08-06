3eb7c2d82d87902194b1fadee1a2457e9e2cdb7b
Moritz Hofmann <info@morihofi.de>
2025-08-06 19:10:13 +0200

    test: Disable long taking test for now

--- certgine-utils/src/test/java/de/morihofi/certgine/utils/scheduler/TimedSchedulerTest.java
+++ certgine-utils/src/test/java/de/morihofi/certgine/utils/scheduler/TimedSchedulerTest.java
@@ -1,5 +1,6 @@
 package de.morihofi.certgine.utils.scheduler;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.time.ZonedDateTime;
@@ -13,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 /**
  * Tests for {@link TimedScheduler} verifying handle cancellation behaviour.
  */
+@Disabled // TODO: Enable again when we filter out long taking tests
 class TimedSchedulerTest {
 
     @Test
