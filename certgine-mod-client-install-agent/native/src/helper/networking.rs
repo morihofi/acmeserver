f98f00f32dfa9733576e3fc3e8406a87bb954360
Moritz Hofmann <info@morihofi.de>
2025-08-24 12:21:52 +0200

    fix(native-agent): use webpki verifier

--- certgine-mod-client-install-agent/native/src/helper/networking.rs
+++ certgine-mod-client-install-agent/native/src/helper/networking.rs
@@ -5,6 +5,8 @@ use base64::write::StrConsumer;
 use reqwest::blocking::{Client, ClientBuilder};
 use rustls::pki_types::CertificateDer;
 use rustls::{ClientConfig, RootCertStore};
+use rustls::client::WebPkiServerVerifier;
+use rustls::crypto::{ring, CryptoProvider};
 use tracing::debug;
 use crate::config::AgentConfig;
 
@@ -17,7 +19,6 @@ pub(crate) fn build_client(agent_config: &AgentConfig) -> Result<reqwest::blocki
         let root_cert_der = base64::prelude::BASE64_URL_SAFE.decode(root_cert_b64url)?;
         let root_cert = CertificateDer::from_slice(&root_cert_der);
 
-
         root_store
             .add(root_cert)
             .expect("failed to add bundled root cert");
@@ -25,7 +26,11 @@ pub(crate) fn build_client(agent_config: &AgentConfig) -> Result<reqwest::blocki
 
     // Build rustls config
     let config = ClientConfig::builder()
-        .with_root_certificates(root_store)
+        //.with_root_certificates(root_store)
+        .with_webpki_verifier(
+            WebPkiServerVerifier::builder_with_provider(<Arc<RootCertStore>>::from(root_store), <Arc<CryptoProvider>>::from(ring::default_provider()))
+                //.with_crls(...)
+                .build()?)
         .with_no_client_auth();
 
 
